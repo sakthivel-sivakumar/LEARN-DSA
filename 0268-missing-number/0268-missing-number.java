f66a068cdd6e4f007782c90f4fe7cb0d2de8684c
sakthivel-sivakumar <158452598+sakthivel-sivakumar@users.noreply.github.com>
2026-02-17 15:35:33 +0530

    Time: 349 ms (7.8%), Space: 47.2 MB (83.29%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,17 +1,24 @@
 class Solution {
     public int missingNumber(int[] arr) {
-        int n = arr.length;
-        for(int i=0;i<=n;i++) // super set(all values)
-        {
-            int c = 0;
-            for(int j=0;j<n;j++) // index 
-            {
-                if(i == arr[j]){
-                    c = 1; break;
-                }
-            }
-            if(c==0) return i;
-        }
-        return -1;
+ 
+
+          int n = arr.length;
+          for(int i=0;i<=n;i++) // super set values  (0,1,2,3)
+          {
+              int c = 0;
+              for(int j=0;j<n;j++) // array indices  [1,3,2]
+              {
+                   if(arr[j] == i)
+                   {
+                     c = c+1;
+                   }
+              } 
+              if(c == 0)
+              return i;
+          } 
+          return -1;
+
+          
+      
     }
 }
