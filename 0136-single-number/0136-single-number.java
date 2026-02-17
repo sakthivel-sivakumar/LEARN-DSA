1bc15be62495b252654ea64ac6e503448c989e0f
sakthivel-sivakumar <158452598+sakthivel-sivakumar@users.noreply.github.com>
2026-02-17 11:59:31 +0530

    Time: 531 ms (6.24%), Space: 46.9 MB (65.07%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,16 +1,17 @@
 class Solution {
-    public int singleNumber(int[] arr) {
-        int n = arr.length;
+    public int singleNumber(int[] arr) {  // 10, 20 , 20, 10 , 30;
+        int n = arr.length; 
         for(int i=0;i<n;i++) 
         {
-            int c = 0;
+            int c = 1;
             for(int j=0;j<n;j++) 
             {
-                if(arr[i] == arr[j]){
-                    c = c+1; 
-                }
+               if(i!= j && arr[i] == arr[j])
+               c++;
             }
-            if(c==1) return arr[i];
+            if(c == 1)
+                return arr[i];
+           
         }
         return -1;
     }
