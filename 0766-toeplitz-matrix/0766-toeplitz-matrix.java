bdf0fb1f65bdda09d743025e2667287e39c08a5f
sakthivel-sivakumar <158452598+sakthivel-sivakumar@users.noreply.github.com>
2026-02-19 15:15:13 +0530

    Time: 0 ms (100%), Space: 46.6 MB (43.99%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -1,16 +1,18 @@
 class Solution {
     public boolean isToeplitzMatrix(int[][] arr) {
-        int m = arr.length;
-        int n = arr[0].length;
 
-        for(int i=1;i<m;i++)
-        {
-            for(int j=1;j<n;j++)
+         int r = arr.length; // row
+         int c = arr[0].length; // col
+
+         for(int i=0;i<r-1;i++)
+         {
+            for(int j=0;j<c-1;j++)
             {
-                if(arr[i][j] != arr[i-1][j-1])
-                    return false;
+                if(arr[i][j] !=  arr[i+1][j+1])
+                return false;
             }
-        }
-        return true;
+         }
+         return true;
+        
     }
 }
