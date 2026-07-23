abf402d7150e4eefa8a24171c16e4819c79db4e2
sakthivel-sivakumar <158452598+sakthivel-sivakumar@users.noreply.github.com>
2026-07-23 10:22:06 +0530

    Time: 0 ms (100%), Space: 48.5 MB (21.59%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -7,12 +7,10 @@ class Solution {
         while(left < right)
         {
             char temp = s[left];
-            s[left]=s[right];
-            s[right]=temp;
+            s[left] = s[right];
+            s[right] = temp;
             left++; right--;
         }
+
     }
 }
-// 9 , 10, 11
-// 8 ,9,10
-// 7,8,9
