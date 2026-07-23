26113f2790446086263c213b6ccb93d4399131ed
sakthivel-sivakumar <158452598+sakthivel-sivakumar@users.noreply.github.com>
2026-07-23 19:57:37 +0530

    Time: 6 ms (78.14%), Space: 45.2 MB (58.6%) - LeetHub

---
+++ 0948-bag-of-tokens/0948-bag-of-tokens.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        Arrays.sort(tokens);
+        int left = 0;
+        int right = tokens.length - 1;
+        int score = 0; int maxScore = 0;
+        while(left <= right){
+            // face-up
+            if(tokens[left] <= power){
+                power-=tokens[left];
+                score++;
+                left++;
+            }
+            // face-down
+            else if(score > 0){
+                power += tokens[right];
+                score--;
+                right--;
+            }
+            else
+                break;
+            
+            maxScore = Math.max(score,maxScore);
+        }
+
+
+        return maxScore;
+        
+
+
+    }
+}
