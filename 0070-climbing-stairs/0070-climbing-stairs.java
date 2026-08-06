61e86caaa12658cc7d535ea0cea8e3731fdff6b7
sakthivel-sivakumar <158452598+sakthivel-sivakumar@users.noreply.github.com>
2026-08-06 11:02:53 +0530

    Time: 0 ms (100%), Space: 41.9 MB (87.55%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int rec(int level,int n,int[] dp){
+        if(level > n)
+            return 0;
+        
+        if(level == n)
+            return 1;
+        
+        if(dp[level] != -1)
+            return dp[level];
+        
+        return dp[level] = rec(level+1 , n,dp) + rec(level+2,n,dp);
+    }
+    public int climbStairs(int n) {
+        int dp[] = new int[n+1];
+        Arrays.fill(dp,-1);
+        return rec(0, n,dp);
+    }
+}
