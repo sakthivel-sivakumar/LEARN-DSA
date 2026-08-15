e229ca7ca2cff842cd5c766ae6a059031f86cd1d
sakthivel-sivakumar <158452598+sakthivel-sivakumar@users.noreply.github.com>
2026-08-15 18:26:50 +0530

    Time: 1 ms (79.84%), Space: 43.5 MB (7.65%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,38 +1,33 @@
 class Solution {
 
+  
     public int numDecodings(String s) {
-
         int n = s.length();
+        int[]dp = new int[n+1];
+        dp[n] = 1;
+        for(int i=n-1;i>=0;i--){
 
-        // dp[n] = 1
-        int next1 = 1; // dp[i + 1]
-        int next2 = 0; // dp[i + 2]
-
-        for (int i = n - 1; i >= 0; i--) {
-
-            int current = 0;
-
-            // Cannot decode starting with 0
-            if (s.charAt(i) != '0') {
-
-                // Take one digit
-                current = next1;
-
-                // Take two digits
-                if (i + 1 < n) {
-
-                    int num = Integer.parseInt(s.substring(i, i + 2));
-
-                    if (num >= 10 && num <= 26)
-                        current += next2;
-                }
+             if (s.charAt(i) == '0') {
+                dp[i] = 0;
+                continue;
             }
 
-            // Move the window
-            next2 = next1;
-            next1 = current;
-        }
+            // Take one digit
+            int one = dp[i+1];
 
-        return next1;
+            // Take two digits
+            int two = 0;
+
+            if (i + 1 < s.length()) {
+
+                int num = Integer.parseInt(s.substring(i, i + 2));
+
+                if (num >= 10 && num <= 26)
+                    two = dp[i+2];
+            }
+
+            dp[i] = one + two;
+        }
+        return dp[0];
     }
 }
